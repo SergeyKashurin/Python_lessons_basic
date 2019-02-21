c7e696601038857c3ccb4785fd46a972d20ef2d6
Sergey <kesha-w810i@ya.ru>
2019-02-21 18:20:49 +0300

    Revert "Revert "Revert "Lesson 3 is done"""
    This reverts commit 92119ebe8af754c37517a279a8ae229523d7fe2c.

--- lesson03/home_work/hw03_easy.py
+++ lesson03/home_work/hw03_easy.py
@@ -4,15 +4,13 @@
 # Округление должно происходить по математическим правилам (0.6 --> 1, 0.4 --> 0).
 # Для решения задачи не используйте встроенные функции и функции из модуля math.
 
-
-# def my_round(number, count):
-#     number = (number * (10 ** count) + 0.41) // 1
-#     return number / (10 ** count)
+def my_round(number, ndigits):
+    pass
 
 
-# print(my_round(2.1234567, 5))
-# print(my_round(2.1999967, 5))
-# print(my_round(2.9999967, 5))
+print(my_round(2.1234567, 5))
+print(my_round(2.1999967, 5))
+print(my_round(2.9999967, 5))
 
 
 # Задание-2:
@@ -21,16 +19,10 @@
 # Билет считается счастливым, если сумма его первых и последних цифр равны.
 # !!!P.S.: функция не должна НИЧЕГО print'ить
 
-# Решение 1
-# def lucky_ticket(ticket_number):
-#     ticket_number_str = str(ticket_number)
-#     ticket_number_lst = [int(i) for i in ticket_number_str]
-#     if len(ticket_number_str) > 5 and (sum(ticket_number_lst[:3]) == sum(ticket_number_lst[-3::])):
-#         return True
-#     else:
-#         return False
-#
-#
-# print(lucky_ticket(123006))
-# print(lucky_ticket(12321))
-# print(lucky_ticket(436751))
+def lucky_ticket(ticket_number):
+    pass
+
+
+print(lucky_ticket(123006))
+print(lucky_ticket(12321))
+print(lucky_ticket(436751))
