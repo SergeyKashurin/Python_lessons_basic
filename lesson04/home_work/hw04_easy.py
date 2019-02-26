e4ceb59149c89ecb47497a822bdaf7fbfd6bf7e4
Sergey <kesha-w810i@ya.ru>
2019-02-26 10:17:01 +0300

    Lesson 4 is done

--- lesson04/home_work/hw04_easy.py
+++ lesson04/home_work/hw04_easy.py
@@ -6,9 +6,34 @@
 # квадратами элементов исходного списка
 # [1, 2, 4, 0] --> [1, 4, 16, 0]
 
+# Решение 1
+# import random
+#
+# lst = [random.randrange(0, 10) for _ in range(5)]
+# print("Исходный список: ", lst)
+#
+#
+# def square_my_lst(loc_lst):
+#     return [i * i for i in loc_lst]
+#
+#
+# print("[DEF] Получившийся список элементов в квадрате: ", square_my_lst(lst))
+#
+# Решение 2
+# # TODO
+# print("[LMD] Получившийся список элементов в квадрате: ", (lambda x: x * x for x in lst))
+
 # Задание-2:
 # Даны два списка фруктов.
 # Получить список фруктов, присутствующих в обоих исходных списках.
+
+# Решение 1
+# fruits_lst1 = ["Apple", "Banana", "Apricot", "Grapes"]
+# fruits_lst2 = ["Apple", "Fig", "Apricot", "Melon"]
+#
+# result_lst = list(set(fruits_lst1) & set(fruits_lst2))
+#
+# print(result_lst)
 
 # Задание-3:
 # Дан список, заполненный произвольными числами.
@@ -16,3 +41,12 @@
 # + Элемент кратен 3
 # + Элемент положительный
 # + Элемент не кратен 4
+
+# Решение 1
+from random import randrange
+
+lst_sample = [randrange(-50, 50) for _ in range(15)]
+print("Исходный список ", lst_sample)
+
+finally_lst = [i for i in lst_sample if i > 0 and i % 3 == 0 and i % 4 != 0]
+print("Результирующий список ", finally_lst)
